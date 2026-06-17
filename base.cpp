f2d386fcfc662a7b41ebdd5edc13cd54bc7a1029
Unforgiven-DEV <23520180@gmi.uit.edu.vn>
2026-06-18 02:17:19 +0700

    The first commit

---
+++ base.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Thử thách 1: In ra tên của bạn
+    cout << "Ten toi la: [Ten cua ban]" << endl;
+    
+    // Thử thách 2: In ra một hình tam giác bằng dấu sao
+    cout << "   *   " << endl;
+    cout << "  ***  " << endl;
+    cout << " ***** " << endl;
+    cout << "*******" << endl;
+    
+    // Thử thách 3: Tính tổng hai số
+    int a = 5;
+    int b = 7;
+    int tong = a + b;
+    cout << "Tong cua " << a << " va " << b << " la: " << tong << endl;
+    
+    return 0;
+}
